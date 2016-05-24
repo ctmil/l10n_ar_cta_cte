2e2e1954cc895b1b64941502793f1a4f433e1f3d
morrillo <gustavo.orrillo@moldeo.coop>
2016-05-24 20:28:29 -0300

    first commit

---
+++ l10n_ar_cta_cte/__openerp__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Localizacion Argentina - Listado Cuentas Corrientes',
+    'category': 'Purchase',
+    'version': '0.1',
+    'depends': ['base','account'],
+    'data': [
+    ],
+    'demo': [
+    ],
+    'qweb': [],
+    'installable': True,
+}

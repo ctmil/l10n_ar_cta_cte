175ce58e2bad85f40c512825c16d4a7deb797f25
morrillo <gustavo.orrillo@moldeo.coop>
2016-05-24 20:51:14 -0300

    Added dependency

--- l10n_ar_cta_cte/__openerp__.py
+++ l10n_ar_cta_cte/__openerp__.py
@@ -2,7 +2,7 @@
     'name': 'Localizacion Argentina - Listado Cuentas Corrientes',
     'category': 'Purchase',
     'version': '0.1',
-    'depends': ['base','account'],
+    'depends': ['base','account','account_report_receivables'],
     'data': [
     ],
     'demo': [
